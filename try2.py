aa43b0964ef11c3b596fa3a854953afdf6089e6c
Farouq Adepetu <84951721+farouqadepetu@users.noreply.github.com>
2022-03-15 18:12:28 -0400

    Update try2.py

--- try2.py
+++ try2.py
@@ -7,10 +7,13 @@ left = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 #right = cv2.VideoCapture(3,cv2.CAP_DSHOW)
 while(True):
    ret,img = left.read()
-   cv2.imshow('video output',img)
-   k=cv2.waitKey(10)& 0xFF
-   if k==37:
-       break
+   if ret == true:
+      cv2.imshow('video output',img)
+      k=cv2.waitKey(10)& 0xFF
+      if k==113:
+          break
+      else:
+         break
 left.release()
 cv2.destroyAllWindows()
       
